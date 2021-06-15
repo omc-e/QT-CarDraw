f2d473c276eeb78fa7943b8b1da995e5a6f3b92e
Emsar <emsar.omic@outlook.com>
2021-06-15 06:28:28 -0700

    added chassis

--- widget.cpp
+++ widget.cpp
@@ -26,8 +26,32 @@ void Widget::paintEvent(QPaintEvent *event)
     cetka1.setColor(Qt::red);
     cetka1.setStyle(Qt::SolidPattern);
 
+    //zuta pozadina
     painter.begin(this);
     painter.fillRect(rect(),Qt::yellow);
+
+    //Sasija
+    QPainterPath spath;
+    spath.moveTo(600, 130);
+        spath.arcTo(QRect(300,40,200,180),0,180);
+        spath.moveTo(300, 130);
+        spath.lineTo(245, 135);
+        spath.lineTo(240, 200);
+        spath.lineTo(290, 197);
+
+        spath.arcTo(QRect(290,155.5,90,90), 180, -180);
+        spath.lineTo(380,205);
+        spath.lineTo(470,205);
+        spath.lineTo(470,205);
+
+        spath.arcTo(QRect(470,155.5,90,90), 180, -180);
+        spath.lineTo(620,200);
+        spath.lineTo(600,130);
+
+    painter.fillPath(spath, Qt::red);
+    painter.drawPath(spath);
+
+
 
 
 }
